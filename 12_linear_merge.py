ac39748feeea2eb3a03e316c7072c300e80aad0b
Pedro Passarelli <pbragapassarelli@yahoo.com.br>
2022-03-27 21:47:47 -0300

    Exercise 12

--- 12_linear_merge.py
+++ 12_linear_merge.py
@@ -10,8 +10,8 @@ A sua solução deve rodar em tempo linear, ou seja, deve fazer uma
 """
 
 def linear_merge(list1, list2):
-    # +++ SUA SOLUÇÃO +++
-    return
+    result = list1 + list2
+    return sorted(result)
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
