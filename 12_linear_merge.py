abe37438568144f33bfc7a45834d88720dcf661a
Pedro Passarelli <pbragapassarelli@yahoo.com.br>
2022-03-30 09:48:37 -0300

    Making exercise 12 a little more elegant

--- 12_linear_merge.py
+++ 12_linear_merge.py
@@ -12,15 +12,13 @@ A sua solução deve rodar em tempo linear, ou seja, deve fazer uma
 def linear_merge(list1, list2):
     result = []
 
-    for i in range(len(list1) + len(list2)):
-        if not list2 or list1[-1] > list2[-1]:
+    while list1:
+        if not list2 or list2[-1] > list1[-1]:
             result.append(list1.pop(-1))
         else:
             result.append(list2.pop(-1))
 
-    result.reverse()
-
-    return result
+    return result[::-1]
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
